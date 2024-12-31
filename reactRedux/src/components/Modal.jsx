3e596f51ac841651386b14791eed5b2a2c0cad76
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-01 00:26:23 +0100

    7.commit

--- reactRedux/src/components/Modal.jsx
+++ reactRedux/src/components/Modal.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Modal = () => {
   return (
-    <div>Modal</div>
-  )
-}
+    <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-screen flex items-center justify-center">
+      <div className="w-1/3 bg-white shadow-lg rounded-md p-4">modal</div>
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
--- reactRedux/src/pages/Product.jsx
+++ reactRedux/src/pages/Product.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import Modal from "../components/Modal";
 
 const Product = () => {
-  const modal = useSelector((state) => state.modal);
+  const { modal } = useSelector((state) => state.modal || {});
 
   console.log(modal, "modal");
 
