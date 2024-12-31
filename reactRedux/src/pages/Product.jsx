4a7e2a3dbbd353cdb6540f89dc86c6c958547c24
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-31 23:58:12 +0100

    sixth commit

--- reactRedux/src/pages/Product.jsx
+++ reactRedux/src/pages/Product.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
 import { useSelector } from "react-redux";
+import Modal from "../components/Modal";
 
 const Product = () => {
-  const { modal } = useSelector((state) => state.modal);
+  const modal = useSelector((state) => state.modal);
 
   console.log(modal, "modal");
+
   return (
     <div>
       <ProductCard />
+      {modal && <Modal />}
     </div>
   );
 };
